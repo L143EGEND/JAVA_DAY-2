d8968f37f0466ce64d6b7be6f74e7b6999b70ea7
Chennakesavulu <134907965+kesavchowdary123@users.noreply.github.com>
2024-11-12 21:10:03 +0530

    Add files via upload

---
+++ Day-5/Buffer.java
@@ -0,0 +1,18 @@
+// string Buffer
+
+class Buffer{
+public static void main(String args[]){
+StringBuffer sb=new StringBuffer ();
+StringBuffer sb1=new StringBuffer ("WELCOME");
+System.out.println("The default capacity of StringBuffer class is:"+sb.capacity());
+System.out.println("The length of given string is:"+sb1.length());
+System.out.println("The reverse of the given string is:"+sb1.reverse());
+}
+}
+
+
+
+
+
+
+
