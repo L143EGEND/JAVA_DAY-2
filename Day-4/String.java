21b12de531a4621efec8b65596affb5664671c25
Chennakesavulu <134907965+kesavchowdary123@users.noreply.github.com>
2024-11-14 20:53:53 +0530

    Add files via upload

---
+++ Day-4/String.java
@@ -0,0 +1,41 @@
+// STRINGS
+
+String s1="kesav"
+String s2="HELLO"
+String s3="Hello"
+System.out.println(s1.length());
+System.out.println(s1.charAt(a));
+System.out.println(s1.indexOf(3));
+System.out.println(s1.toUpperCase());
+System.out.println(s2.toLowerCase());
+System.out.println(s2.replace('H','C'));
+
+if(s1.euals int(s2))
+System.out.println("Both are equals");
+else
+System.out.println("Both are not equals");
+
+if(s3.eualsignorecase(s2))
+System.out.println("Both are equals");
+else
+System.out.println("Both are not equals");
+
+public class Main {
+public static void main(string arge[]) {
+String s1="kesav"
+for(int i=s1.length();i>0:i--); {
+System.out.println(s1.charAt(i-1));
+}}}
+
+
+// String Buffer
+
+class buffer {
+public static void main(string arge[]) {
+StringBuffer sb=new StringBuffer();
+StringBuffer sb1=new StringBuffer("WELCOME");
+System.out.println("the default capacity of SB is:"+sb.capacity());
+System.out.println("the length of string is:"+sb.length());
+System.out.println("the reverse of string is:"+sb.reverse());
+
+
