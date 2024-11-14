215b63f4e1d55ef5417c87e0ff65283b3e217858
Chennakesavulu <134907965+kesavchowdary123@users.noreply.github.com>
2024-11-14 21:11:13 +0530

    Add files via upload

---
+++ Day-5/Additi.java
@@ -0,0 +1,20 @@
+// Addition of two number by using user defined method (with return value, with argument value)
+
+class Addit {
+int a, b, c;
+void add(int X, int Y) { 
+a=X; 
+b=Y; 
+c=a+b;
+return c;
+}
+}
+class Add {
+public static void main(String args[]) {
+int m=23,n=55,t;
+Addit a1=new Addit();
+t=a1.add(m,n);
+System.out.println("The addition value is:"+c);
+
+}
+}
