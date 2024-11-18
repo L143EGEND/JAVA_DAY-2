76ec3b4f593fb2fddce994d7e022451d089e1952
Chennakesavulu <134907965+kesavchowdary123@users.noreply.github.com>
2024-11-18 20:28:45 +0530

    Add files via upload

---
+++ Day-9/Expe.java
@@ -0,0 +1,26 @@
+// exception Handling
+
+class ArthException {
+int a, b, c;
+void Divide() {
+try {
+a=10;
+b=0;
+c=a/b;
+
+System.out.println("The Divison Value is:"+c);
+}
+catch (ArithmeticException ae) {
+
+System.out.println("The Divison Error is Occuried");
+//System.out.println("The Divison Error is Occuried"+ae.getMessage);
+System.out.println("The Divison Error is Occuried"+ae);
+}
+}
+}
+class Expe {
+public static void main(String args[]) {
+ArthException a1 = new ArthException ();
+a1.Divide();
+}
+}
